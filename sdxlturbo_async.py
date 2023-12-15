24083e944ca9fd3a9716e4f4a017f61e13ba6f9a
Goncalo Guiomar <goncalo.guiomar@gmail.com>
2023-12-15 14:45:09 +0000

    started async prompt blending

---
+++ sdxlturbo_async.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 22:13:24 2023
+
+@author: lunar
+"""
+
+#%%
+from diffusers import AutoPipelineForText2Image
+import torch
+import time
+
+from diffusers import AutoencoderTiny
+# this line breaks computers
+# from sfast.compilers.stable_diffusion_pipeline_compiler import (compile, CompilationConfig)
+import xformers
+import triton
+import lunar_tools as lt
+from PIL import Image
+import numpy as np
+from diffusers.utils.torch_utils import randn_tensor
+import random as rn
+import numpy as np
+import asyncio
+
+from prompt_blender import PromptBlender
+from prompt_blender import PromptBlenderAsync
+
+torch.set_grad_enabled(False)
+torch.backends.cuda.matmul.allow_tf32 = False
+torch.backends.cudnn.allow_tf32 = False
+
+
+pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+pipe = pipe.to("cuda")
+pipe.vae = AutoencoderTiny.from_pretrained('madebyollin/taesdxl', torch_device='cuda', torch_dtype=torch.float16)
+pipe.vae = pipe.vae.cuda()
+pipe.set_progress_bar_config(disable=True)
+
+async def prompt_input_loop(blender, update_event):
+    while True:
+        new_prompt = await asyncio.to_thread(input, "Enter new prompt: ")
+        blender.add_prompt(new_prompt)
+        update_event.set()  # Signal that a new prompt has been added
+
+async def render_loop(blender, pipe, update_event):
+    sz = (512, 512)
+    renderer = lt.Renderer(width=sz[1], height=sz[0])
+    latents = torch.randn((1, 4, 64, 64)).half().cuda()
+
+    while True:
+        # Check if there's a new prompt to blend
+        if update_event.is_set():
+            update_event.clear()  # Reset the event
+
+        # Continue rendering with the current blended prompts
+        for i in range(len(blender.blended_prompts) - 1):
+            fract = float(i) / (len(blender.blended_prompts) - 1)
+            blended = blender.blend_prompts(blender.blended_prompts[i], blender.blended_prompts[i+1], fract)
+
+            prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blended
+            image = pipe(guidance_scale=0.0, num_inference_steps=1, latents=latents, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
+            renderer.render(image.rotate(90))
+
+        await asyncio.sleep(0.1)  # Short delay to yield control
+
+async def main():
+    initial_prompts = ["initial prompt 1", "initial prompt 2"]
+    n_steps = 100
+    blender = PromptBlenderAsync(pipe, initial_prompts, n_steps)
+    update_event = asyncio.Event()
+
+    # Start the asynchronous loops
+    input_task = asyncio.create_task(prompt_input_loop(blender, update_event))
+    render_task = asyncio.create_task(render_loop(blender, pipe, update_event))
+
+    # Run both tasks concurrently
+    await asyncio.gather(input_task, render_task)
+
+# Get the current event loop and run the main function
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
