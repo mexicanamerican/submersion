219ac157802fa8ec0ccad03bd7dd5a7b093566f8
Johannes Stelzer <jsdmail@gmail.com>
2023-12-21 17:09:23 +0100

    first version ctrlnet with webcam

--- sdxlturbo_ctrlnet.py
+++ sdxlturbo_ctrlnet.py
@@ -1,122 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 #%%
-from diffusers import AutoPipelineForText2Image
+from diffusers import AutoPipelineForText2Image, AutoPipelineForImage2Image
 import torch
 import time
-
 from diffusers import AutoencoderTiny
-#from sfast.compilers.stable_diffusion_pipeline_compiler import (compile, CompilationConfig)
+from sfast.compilers.stable_diffusion_pipeline_compiler import (compile, CompilationConfig)
+from diffusers.utils import load_image
 import xformers
 import triton
 import lunar_tools as lt
 from PIL import Image
 import numpy as np
 from diffusers.utils.torch_utils import randn_tensor
+import random as rn
+import numpy as np
+import xformers
+import triton
+import cv2
+from diffusers import StableDiffusionXLControlNetPipeline, ControlNetModel, AutoencoderKL
+from prompt_blender import PromptBlender
+
+shape_cam=(600,800) 
+cam_man = lt.WebCam(cam_id=0, shape_hw=shape_cam)
+cam_man.cam.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+
 
 torch.set_grad_enabled(False)
 torch.backends.cuda.matmul.allow_tf32 = False
 torch.backends.cudnn.allow_tf32 = False
 
-
-# pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
-# pipe = pipe.to("cuda")
-
-# pipe.vae = AutoencoderTiny.from_pretrained('madebyollin/taesdxl', torch_device='cuda', torch_dtype=torch.float16)
-# pipe.vae = pipe.vae.cuda()
-
-# pipe.set_progress_bar_config(disable=True)
-
-# pipe = compile(pipe, config)
-
-
-#%%
-
-# Example usage
-prompts = ["a man walking through the forest", "a man walking through the desert", "a man walking through the village", "a man walking through the war in the village","a man walking through the war in the village with explosions","a man walking through the destructed village, dead bodies, gore" , "a man walking through the desert", "a man walking through the forest"]
-n_steps = 100
-# blended_prompts = blend_sequence_prompts(prompts, n_steps)
-
-
-
-#%%
-sz = (512, 512)
-# image = pipe(prompt="photo of a man", guidance_scale=0.0, num_inference_steps=4).images[0]
-
-
-#%% 
-
-from diffusers.utils import load_image
-import cv2
-from PIL import Image
-import numpy as np
-
-
-image = load_image(
-    "https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png"
-)
-image
-image = image.resize(512, 512)
-
-image = np.array(image)
-
-low_threshold = 100
-high_threshold = 200
-
-image = cv2.Canny(image, low_threshold, high_threshold)
-image = image[:, :, None]
-image = np.concatenate([image, image, image], axis=2)
-canny_image = Image.fromarray(image)
-
-#%%
-from diffusers import StableDiffusionXLControlNetPipeline, ControlNetModel, AutoencoderKL
-
-from diffusers.utils import load_image
-
-import numpy as np
-
-import torch
-
-import cv2
-
-from PIL import Image
-
-prompt = "aerial view, a futuristic research complex in a bright foggy jungle, hard lighting"
-
-negative_prompt = "low quality, bad quality, sketches"
-
-# download an image
-
-image = load_image(
-
-    "https://hf.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/hf-logo.png"
-
-)
+use_maxperf = False
 
 # initialize the models and pipeline
-
-controlnet_conditioning_scale = 0.0  # recommended for good generalization
-
-controlnet = ControlNetModel.from_pretrained(
-
-    "diffusers/controlnet-canny-sdxl-1.0", torch_dtype=torch.float16
-    )
-
-
-#%%
-# from diffusers import StableDiffusionControlNetPipeline, ControlNetModel
-# import torch
-# controlnet = ControlNetModel.from_pretrained("lllyasviel/diffusers_xl_depth_full", torch_dtype=torch.float16)
-#%%
+ctrlnet_type = "diffusers/controlnet-canny-sdxl-1.0"
+controlnet = ControlNetModel.from_pretrained(ctrlnet_type, torch_dtype=torch.float16)
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", controlnet=controlnet, torch_dtype=torch.float16, variant="fp16")
-# xxx
-# pipe = StableDiffusionControlNetPipeline.from_pretrained(
-#     "stabilityai/sdxl-turbo", controlnet=controlnet, torch_dtype=torch.float16, variant="fp16"
-# )
 pipe = pipe.to("cuda")
+pipe.vae = AutoencoderTiny.from_pretrained('madebyollin/taesdxl', torch_device='cuda', torch_dtype=torch.float16)
+pipe.vae = pipe.vae.cuda()
+pipe.set_progress_bar_config(disable=True)
 
+
+if use_maxperf:
+    config = CompilationConfig.Default()
+    config.enable_xformers = True
+    config.enable_triton = True
+    config.enable_cuda_graph = True
+    config.enable_jit = True
+    config.enable_jit_freeze = True
+    config.trace_scheduler = True
+    config.enable_cnn_optimization = True
+    config.preserve_parameters = False
+    config.prefer_lowp_gemm = True
+    pipe = compile(pipe, config)
 
 #%%
+def process_cam_image(ctrl_image):
+    ctrl_image = center_crop_and_resize(ctrl_image)
+    ctrl_image = np.array(ctrl_image)
+    low_threshold = 100
+    high_threshold = 200
+    ctrl_image = cv2.Canny(ctrl_image, low_threshold, high_threshold)
+    ctrl_image = ctrl_image[:, :, None]
+    ctrl_image = np.concatenate([ctrl_image, ctrl_image, ctrl_image], axis=2)
+    ctrl_image = Image.fromarray(ctrl_image)
+    return ctrl_image
+
+def center_crop_and_resize(img, size=(512, 512)):
+    """
+    Center crop an image to the specified size and resize it.
+
+    Args:
+        input_image_path (str): Path to the input image.
+        output_image_path (str): Path to save the cropped and resized image.
+        size (tuple): Target size in the format (width, height). Default is (512, 512).
+    """
+    try:
+        # Get the original dimensions of the image
+        width, height = img.size
+
+        # Calculate the coordinates for the center crop
+        left = (width - size[0]) / 2
+        top = (height - size[1]) / 2
+        right = (width + size[0]) / 2
+        bottom = (height + size[1]) / 2
+
+        # Crop the image
+        cropped_img = img.crop((left, top, right, bottom))
+
+        # Resize the cropped image to the specified size
+        cropped_img = cropped_img.resize(size)
+
+        return cropped_img
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Usage example:
+# center_crop_and_resize("your_image.jpg", "output_image.jpg", size=(512, 512))
+
+# Example usage
+blender = PromptBlender(pipe)
+
+from nltk.corpus import wordnet as wn
+nouns = list(wn.all_synsets('n'))
+nouns = [nouns[i].lemma_names()[0] for i in range(len(nouns))]
+
+# base = 'skeleton person head skull terrifying'
+base = 'very bizarre and grotesque zombie monster'
+
+tp = 150
+prompts = []
+for i in range(tp):
+    prompts.append(f'A painting of {base} who looks like {nouns[np.random.randint(len(nouns))]}. 4K Ultra HD. A lot of detail')
+
+n_steps = 30
+blended_prompts = blender.blend_sequence_prompts(prompts, n_steps)
+
+# Image generation pipeline
 controlnet_conditioning_scale = 0.5
-t0 = time.time()
-image = pipe("photo of a man", image=canny_image, controlnet_conditioning_scale=controlnet_conditioning_scale, guidance_scale=0.0, num_inference_steps=4).images[0]
-dt = time.time() - t0
-print(f"took {dt}s")
+sz = (512*2, 512*2)
+renderer = lt.Renderer(width=sz[1], height=sz[0])
+latents = torch.randn((1,4,64//1,64)).half().cuda()
+
+# Iterate over blended prompts
+while True:
+    print('restarting...')
+    
+    for i in range(len(blended_prompts) - 1):
+        torch.manual_seed(1)
+        
+        fract = float(i) / (len(blended_prompts) - 1)
+        blended = blender.blend_prompts(blended_prompts[i], blended_prompts[i+1], fract)
+    
+        prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blended
+        
+        cam_img = cam_man.get_img()
+        cam_img = np.flip(cam_img, axis=1)
+        cam_img = Image.fromarray(np.uint8(cam_img))
+        
+        ctrl_img = process_cam_image(cam_img)
+        
+        
+        # Generate the image using your pipeline
+        # image = pipe(image=image, latents=latents, num_inference_steps=2, strength=0.999, guidance_scale=0.5, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
+        image = pipe(image=ctrl_img, latents=latents, controlnet_conditioning_scale=controlnet_conditioning_scale, guidance_scale=0.0, num_inference_steps=4, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
+        # Render the image
+        renderer.render(image)
+        
