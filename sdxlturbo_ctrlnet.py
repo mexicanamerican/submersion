3ef030e5638cf806b4c491547145b8521a5d3c04
Johannes Stelzer <jsdmail@gmail.com>
2023-12-23 17:06:53 +0100

    depth filtering

--- sdxlturbo_ctrlnet.py
+++ sdxlturbo_ctrlnet.py
@@ -22,7 +22,7 @@ import cv2
 from diffusers import StableDiffusionXLControlNetPipeline, ControlNetModel, AutoencoderKL
 from prompt_blender import PromptBlender
 from transformers import DPTFeatureExtractor, DPTForDepthEstimation
-from img_utils import pad_image_to_width, pad_image_to_width, blend_images, process_cam_img
+from img_utils import pad_image_to_width, pad_image_to_width, blend_images, process_cam_img, stitch_images, weighted_average_images
 
 #%% PARAMETERS
 
@@ -30,7 +30,7 @@ ctrlnet_type = "diffusers/controlnet-canny-sdxl-1.0"
 # ctrlnet_type = "diffusers/controlnet-depth-sdxl-1.0"
 use_maxperf = False
 shape_cam=(600,800)
-size_ctrl_img = (512, 512) 
+size_diff_img = (512, 512) 
 num_inference_steps = 2
 controlnet_conditioning_scale = 0.45
 stitch_cam = False
@@ -43,9 +43,8 @@ torch.set_grad_enabled(False)
 torch.backends.cuda.matmul.allow_tf32 = False
 torch.backends.cudnn.allow_tf32 = False
 
-if "depth" in ctrlnet_type:
-    depth_estimator = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas").to("cuda")
-    feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-hybrid-midas")
+depth_estimator = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas").to("cuda")
+feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-hybrid-midas")
 
 # initialize the models and pipeline
 controlnet = ControlNetModel.from_pretrained(
@@ -75,30 +74,21 @@ if use_maxperf:
     pipe = compile(pipe, config)
 
 #%%
-def get_ctrl_img(ctrl_image, ctrlnet_type):
-    ctrl_image = np.array(ctrl_image)
+def get_ctrl_img(cam_img, ctrlnet_type):
+    
     
     if ctrlnet_type == "diffusers/controlnet-canny-sdxl-1.0":
+        cam_img = np.array(cam_img)
         low_threshold = 100
         high_threshold = 200
-        ctrl_image = cv2.Canny(ctrl_image, low_threshold, high_threshold)
+        ctrl_image = cv2.Canny(cam_img, low_threshold, high_threshold)
         ctrl_image = ctrl_image[:, :, None]
         ctrl_image = np.concatenate([ctrl_image, ctrl_image, ctrl_image], axis=2)
         ctrl_image = Image.fromarray(ctrl_image)
     else:
-        image = feature_extractor(images=ctrl_image, return_tensors="pt").pixel_values.to("cuda")
-        with torch.no_grad(), torch.autocast("cuda"):
-            depth_map = depth_estimator(image).predicted_depth
-    
-        depth_map = torch.nn.functional.interpolate(
-            depth_map.unsqueeze(1),
-            size=size_ctrl_img,
-            mode="bicubic",
-            align_corners=False,
-        )
-        depth_min = torch.amin(depth_map, dim=[1, 2, 3], keepdim=True)
-        depth_max = torch.amax(depth_map, dim=[1, 2, 3], keepdim=True)
-        depth_map = (depth_map - depth_min) / (depth_max - depth_min)
+        depth_map = get_depth_map(cam_img)
+
+        
         image = torch.cat([depth_map] * 3, dim=1)
     
         image = image.permute(0, 2, 3, 1).cpu().numpy()[0]
@@ -107,6 +97,27 @@ def get_ctrl_img(ctrl_image, ctrlnet_type):
         
     return ctrl_image    
 
+def get_depth_map(cam_img, return_type="pt"):
+    cam_img = np.array(cam_img)
+    image = feature_extractor(images=cam_img, return_tensors="pt").pixel_values.to("cuda")
+    with torch.no_grad(), torch.autocast("cuda"):
+        depth_map = depth_estimator(image).predicted_depth
+        
+    depth_map = torch.nn.functional.interpolate(
+        depth_map.unsqueeze(1),
+        size=size_diff_img,
+        mode="bicubic",
+        align_corners=False,
+    )
+    depth_min = torch.amin(depth_map, dim=[1, 2, 3], keepdim=True)
+    depth_max = torch.amax(depth_map, dim=[1, 2, 3], keepdim=True)
+    depth_map = (depth_map - depth_min) / (depth_max - depth_min)
+    
+    if return_type == "np":
+        depth_map = depth_map.cpu().numpy()[0][0]
+        depth_map = depth_map.clip(0, 1)
+    
+    return depth_map
 
 
 #%%
@@ -134,7 +145,7 @@ prompt = 'a medieval scene with people dressed in strange clothes'
 # base = 'a gangster party'
 
 
-sz = (size_ctrl_img[0]*2, size_ctrl_img[1]*2)
+sz = (size_diff_img[0]*2, size_diff_img[1]*2)
 width_renderer = width=2*sz[1]
 
 renderer = lt.Renderer(width=width_renderer, height=sz[0])
@@ -147,7 +158,7 @@ speech_detector = lt.Speech2Text()
 
 akai_lpd8 = lt.MidiInput("akai_lpd8")
 
-image_diffusion = Image.fromarray(np.zeros((size_ctrl_img[1], size_ctrl_img[0], 3), dtype=np.uint8))
+image_diffusion = Image.fromarray(np.zeros((size_diff_img[1], size_diff_img[0], 3), dtype=np.uint8))
 img_buffer = []
 while True:
     torch.manual_seed(1)
@@ -156,10 +167,11 @@ while True:
     
     controlnet_conditioning_scale = akai_lpd8.get("E0")
 
-    if akai_lpd8.get("A0", button_mode="is_pressed"):
+    do_record = akai_lpd8.get("A0", button_mode="is_pressed")
+    if do_record:
         if not speech_detector.audio_recorder.is_recording:
             speech_detector.start_recording()
-    elif not akai_lpd8.get("A0", button_mode="is_pressed"):
+    elif not do_record:
         if speech_detector.audio_recorder.is_recording:
             try:
                 prompt = speech_detector.stop_recording()
@@ -170,22 +182,19 @@ while True:
             stop_recording = False
 
     cam_img = process_cam_img(cam_man.get_img())
+    
+    do_depth_filter = akai_lpd8.get("C0", button_mode="toggle")
+    if do_depth_filter:
+        depth_map = get_depth_map(cam_img, return_type='np')
+        threshold_depth = akai_lpd8.get("F0", val_default=0.7)
+        cam_img_np = np.array(cam_img)
+        cam_img_np[depth_map < threshold_depth] = 0
+        cam_img = Image.fromarray(cam_img_np)
+    # ctrl_img_diffusion = get_ctrl_img(image_diffusion, ctrlnet_type)
+    # fract_mixing = akai_lpd8.get("E1", val_default=0)
+    
     ctrl_img_cam = get_ctrl_img(cam_img, ctrlnet_type)
-    
-    if akai_lpd8.get("C0", button_mode="toggle"):
-        nmb_img_buffers = int(akai_lpd8.get("F0", val_min=1, val_max=10, val_default=3))
-        img_buffer.append(ctrl_img_cam)
-        if len(img_buffer) > nmb_img_buffers:
-            img_buffer.pop(0)
-        
-        cam_img = Image.fromarray(np.mean([np.array(img) for img in img_buffer], axis=0).astype(np.uint8))
-        ctrl_img_cam = weighted_average_images(img_buffer, 0.3, 1.0)
-    
-    ctrl_img_diffusion = get_ctrl_img(image_diffusion, ctrlnet_type)
-    
-    fract_mixing = akai_lpd8.get("E1", val_default=0)
-    
-    ctrl_img = blend_images(ctrl_img_cam, ctrl_img_diffusion, fract_mixing)
+    ctrl_img = ctrl_img_cam #blend_images(ctrl_img_cam, ctrl_img_diffusion, fract_mixing)
 
     image_diffusion = pipe(image=ctrl_img, latents=latents, controlnet_conditioning_scale=controlnet_conditioning_scale, guidance_scale=0.0, num_inference_steps=num_inference_steps, prompt_embeds=prompt_embeds, negative_prompt_embeds=negative_prompt_embeds, pooled_prompt_embeds=pooled_prompt_embeds, negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
     
