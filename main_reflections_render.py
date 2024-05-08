a8ac197b80f93e7186282fd7d67fab51eee1ee56
lugo <jsdmail@gmail.com>
2024-05-08 14:14:00 +0100

    bugfix strength

--- main_reflections_render.py
+++ main_reflections_render.py
@@ -32,7 +32,6 @@ torch.backends.cudnn.allow_tf32 = False
 import matplotlib.pyplot as plt
 from huggingface_hub import hf_hub_download
 from ultralytics import YOLO
-from supervision import Detections
 import random
 
 model_path = hf_hub_download(repo_id="arnabdhar/YOLOv8-Face-Detection", filename="model.pt")
@@ -40,9 +39,11 @@ yolo = YOLO(model_path)
 #%% PARAMETERS
 compile_with_sfast = False
 shape_cam= (1080, 1920)
+# shape_cam= (720, 1280)
+precrop_shape_cam = (750, 750)
 size_diff_img = (512, 512)
-size_render = shape_cam #(1024, 1024) 
-padding = 70
+size_render = (1080, int(1080*precrop_shape_cam[1]/precrop_shape_cam[0])) 
+padding = 120
 human_mask_boundary_relative = 0.1
 guidance_scale = 0.0
 # %% INITS
@@ -55,14 +56,28 @@ pipe = pipe.to("cuda")
 # pipe.vae = pipe.vae.cuda()
 pipe.set_progress_bar_config(disable=True)
 
-if compile_with_sfast:
-    from sfast.compilers.diffusion_pipeline_compiler import (compile, CompilationConfig)
-    pipe.enable_xformers_memory_efficient_attention()
-    config = CompilationConfig.Default()
-    config.enable_xformers = True
-    config.enable_triton = True
-    config.enable_cuda_graph = True
-    pipe = compile(pipe, config)
+# if compile_with_sfast:
+#     pipe.enable_xformers_memory_efficient_attention()
+#     config = CompilationConfig.Default()
+#     config.enable_xformers = True
+#     config.enable_triton = True
+#     config.enable_cuda_graph = True
+#     pipe = compile(pipe, config)
+    
+    
+# if compile_with_sfast:
+#     from sfast.compilers.diffusion_pipeline_compiler import (compile, CompilationConfig)
+#     config = CompilationConfig.Default()
+#     config.enable_xformers = True
+#     config.enable_triton = True
+#     config.enable_cuda_graph = True
+#     config.enable_jit = True
+#     config.enable_jit_freeze = True
+#     config.trace_scheduler = True
+#     config.enable_cnn_optimization = True
+#     config.preserve_parameters = False
+#     config.prefer_lowp_gemm = True
+#     pipe = compile(pipe, config)
 
 # %% aux
 def _compute_zero_padding(kernel_size: Tuple[int, int]) -> Tuple[int, int]:
@@ -236,22 +251,48 @@ meta_input = lt.MetaInput()
 
 #%%
 
+def get_prompt():
+    # return get_prompt_celebrity()
+    # return get_prompt_emo()
+    return get_prompt_nationalities_age_emotion()
 
-list_nationalities = ["nigerian", "moroccan", "turkish", "persian", "spanish", "american", "japanese", "chinese", "mongolian", "russian", "Brazilian", "Australian", "Egyptian", "Finnish", "Canadian", "Argentinian", "South Korean", "Kenyan", "Ukrainian", "Norwegian"]
-list_emotions = ["angry", "sad", "surprised", "happy", "smiling", "furious", "Ecstatic", "Melancholic", "Bewildered", "Content", "Nostalgic", "Anxious"]
-list_ages = ["middle aged", "very old", "very young", "teenager", "Toddler", "Elderly", "Young adult", "In their thirties"]
 
-nationality = random.choice(list_nationalities)
-emotion = random.choice(list_emotions)
-age = random.choice(list_ages)
-prompt = f"photo of a {emotion} and {age} {nationality} person"
+def get_prompt_nationalities_age_emotion():
+    list_nationalities = ["nigerian", "moroccan", "turkish", "persian", "spanish", "american", "japanese", "chinese", "mongolian", "russian", "Brazilian", "Australian", "Egyptian", "Finnish", "Canadian", "Argentinian", "South Korean", "Kenyan", "Ukrainian", "Norwegian"]
+    list_emotions = ["angry", "sad", "surprised", "happy", "smiling", "furious", "Ecstatic", "Melancholic", "Bewildered", "Content", "Nostalgic", "Anxious"]
+    list_ages = ["middle aged", "very old", "very young", "teenager", "Toddler", "Elderly", "Young adult", "In their thirties"]
+    
+    nationality = random.choice(list_nationalities)
+    emotion = random.choice(list_emotions)
+    age = random.choice(list_ages)
+    gender = random.choice(['male', 'female'])
+    prompt = f"photo of a {emotion} and {age} {nationality} {gender} person"
+    return prompt
 
-t_transform_started = 0
+
+def get_prompt_celebrity():
+    list_celebs = [
+        'donald trump', 'elon musk', 'Oprah Winfrey', 'Tom Cruise', 'Taylor Swift', 'Leonardo DiCaprio',
+        'Beyoncé', 'Brad Pitt', 'Emma Watson', 'Cristiano Ronaldo', 'Angelina Jolie', 'Roger Federer',
+        'Kim Kardashian', 'Justin Bieber', 'Serena Williams', 'Keanu Reeves', 'Ariana Grande', 'Will Smith',
+        'Rihanna', 'Johnny Depp', 'Jennifer Lawrence', 'Lionel Messi', 'Scarlett Johansson', 'Shakira',
+        'Dwayne Johnson', 'Lady Gaga', 'Robert Downey Jr.', 'Katy Perry', 'LeBron James', 'Adele'
+    ]
+
+    prompt = f"photo of {random.choice(list_celebs)}"
+    return prompt
+
+def get_prompt_emo():
+    emotion = random.choice(['happy', 'sad'])
+    prompt = f"close-up portrait of a {emotion} person"
+    return prompt
+
+
 
 #%%
 human_mask_boundary = int(human_mask_boundary_relative*min(size_diff_img))
 is_transformation_active = False
-
+list_scores = []
 
 prompt = "photo of a very old and very angry american person"
 prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blender.get_prompt_embeds(prompt, negative_prompt)
@@ -261,22 +302,26 @@ t_transform_started = time.time()
 is_active_transform = False
 get_new_prompt = False
 num_inference_steps = 30
+t_transform_started = 0
 
 while True:
+    t0 = time.time()
         
-    dt_transform_in = meta_input.get(akai_midimix="A0", val_min=0, val_max=10)
-    dt_transform_stay = meta_input.get(akai_midimix="A1", val_min=0, val_max=10)
-    dt_transform_out = meta_input.get(akai_midimix="A2", val_min=0, val_max=10)
+    dt_transform_in = meta_input.get(akai_midimix="A0", val_min=1, val_max=20)
+    dt_transform_stay = meta_input.get(akai_midimix="A1", val_min=1, val_max=20)
+    dt_transform_out = meta_input.get(akai_midimix="A2", val_min=1, val_max=20)
     
     num_inference_steps_min = meta_input.get(akai_midimix="B0", val_min=2, val_max=5, val_default=4)
-    num_inference_steps_max = meta_input.get(akai_midimix="B1", val_min=6, val_max=30, val_default=30)
+    num_inference_steps_max = meta_input.get(akai_midimix="B1", val_min=6, val_max=50, val_default=50)
     
-    p_start_transform = -1 #meta_input.get(akai_midimix="B0", val_min=0, val_max=0.1)
+    p_start_transform = meta_input.get(akai_midimix="B2", val_min=0, val_max=0.1)
+    
+    manual_strength_overrider = meta_input.get(akai_midimix='C4', button_mode='toggle')
 
-    if not is_active_transform and np.random.rand() < p_start_transform:
+    if not is_active_transform and not manual_strength_overrider and np.random.rand() < p_start_transform:
         start_transform = True
         
-    start_transform_manually = meta_input.get(akai_midimix='B3', akai_lpd8='A0', button_mode='pressed_once')
+    start_transform_manually = meta_input.get(akai_midimix='C3', akai_lpd8='A0', button_mode='pressed_once')
     if start_transform_manually:
         start_transform = True
         
@@ -285,6 +330,16 @@ while True:
         get_new_prompt = True
         start_transform = False
         is_active_transform = True
+        did_print_in = False
+        did_print_stay = False
+        did_print_out = False
+        print(f"start_transform True! {dt_transform_in} {dt_transform_stay} {dt_transform_out}")
+        
+    if get_new_prompt:
+        prompt = get_prompt()
+        print(f"new prompt: {prompt}")
+        prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blender.get_prompt_embeds(prompt, negative_prompt)
+        get_new_prompt = False
         
     if is_active_transform:
         dt_transform = time.time() - t_transform_started
@@ -293,32 +348,44 @@ while True:
         if dt_transform >= dt_transform_in + dt_transform_stay + dt_transform_out:
             is_active_transform = False
             print(f"phase: transform ended, back to normal")
+            num_inference_steps = num_inference_steps_max
         elif dt_transform >= dt_transform_in + dt_transform_stay:
             fract_transform = (dt_transform - dt_transform_in - dt_transform_stay) / dt_transform_out
             num_inference_steps = fract_transform*(num_inference_steps_max-num_inference_steps_min) + num_inference_steps_min
-            print(f"phase: transform out: fract_transform {fract_transform} num_inference_steps {num_inference_steps}")
+            # print(f"phase: transform out: fract_transform {fract_transform} num_inference_steps {num_inference_steps}")
+            if not did_print_out:
+                print("phase: starting transform back")
+                did_print_out = True
         elif dt_transform >= dt_transform_in:
             fract_transform = (dt_transform - dt_transform_in) / dt_transform_stay
             num_inference_steps = num_inference_steps_min
-            print(f"phase: transform stay: fract_transform {fract_transform} num_inference_steps {num_inference_steps}")
+            if not did_print_stay:
+                print("phase: starting staying transformed")
+                did_print_stay = True
+            # print(f"phase: transform stay: fract_transform {fract_transform} num_inference_steps {num_inference_steps}")
         else:
-            fract_transform = dt_transform / dt_transform_stay
+            fract_transform = dt_transform / dt_transform_in
             num_inference_steps = (1-fract_transform)*(num_inference_steps_max-num_inference_steps_min) + num_inference_steps_min
-            print(f"phase: transform in: fract_transform {fract_transform} num_inference_steps {num_inference_steps}")
+            # print(f"phase: transform in: fract_transform {fract_transform} num_inference_steps {num_inference_steps}")
+            if not did_print_in:
+                print("phase: starting transform")
+                did_print_in = True
         
         num_inference_steps = np.clip(num_inference_steps, num_inference_steps_min, num_inference_steps_max)
         num_inference_steps = int(np.round(num_inference_steps))
+        
+    manual_num_inference_steps = meta_input.get(akai_midimix='C5', val_min=num_inference_steps_min, val_max=num_inference_steps_max)
     
-    if get_new_prompt:
-        nationality = random.choice(list_nationalities)
-        emotion = random.choice(list_emotions)
-        age = random.choice(list_ages)  
-        prompt = f"photo of a {emotion} and {age} {nationality} person"
-        print(f"new prompt: {prompt}")
-        prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds = blender.get_prompt_embeds(prompt, negative_prompt)
-        get_new_prompt = False
+    if manual_strength_overrider:
+        num_inference_steps =int(manual_num_inference_steps)
+        
     
-    t0 = time.time()
+    psychophysics_detection = meta_input.get(akai_midimix="H4", button_mode="pressed_once")
+    if psychophysics_detection:
+        print(f"captured at fract_transform {fract_transform}")
+        list_scores.append(fract_transform)
+    
+    
     torch.manual_seed(420)
     
     # num_inference_steps = int(meta_input.get(akai_midimix="B2", akai_lpd8="H1", val_min=3, val_max=30, val_default=30))
@@ -326,6 +393,7 @@ while True:
     cam_img = cam.get_img()
     cam_img = np.flip(cam_img, axis=1)
     cam_img = np.uint8(cam_img)
+    cam_img = cam_img[(cam_img.shape[0] - precrop_shape_cam[0]) // 2:(cam_img.shape[0] + precrop_shape_cam[0]) // 2, (cam_img.shape[1] - precrop_shape_cam[1]) // 2:(cam_img.shape[1] + precrop_shape_cam[1]) // 2]
     
     cropping_coordinates = crop_face_square(cam_img, padding=150)
     if cropping_coordinates is None:
@@ -339,26 +407,31 @@ while True:
     # only apply human mask in cam_img_cropped  
     human_mask = human_seg.get_mask(np.asarray(cam_img_cropped))
 
-    strength = 1/num_inference_steps + 0.00001
-    image_diffusion = pipe(image=cam_img_cropped, 
+    strength = 1/num_inference_steps + 0.0001
+    
+    acid_strength = meta_input.get(akai_midimix="C0", val_min=0, val_max=0.8, val_default=0.11)
+    input_image_diffusion = cam_img_cropped
+    # input_image_diffusion = blend_images(image_diffusion, cam_img_cropped, acid_strength)
+    
+    image_diffusion = pipe(image=input_image_diffusion, 
                     latents=latents, num_inference_steps=num_inference_steps, strength=strength, 
                     guidance_scale=guidance_scale, prompt_embeds=prompt_embeds, 
                     negative_prompt_embeds=negative_prompt_embeds, 
                     pooled_prompt_embeds=pooled_prompt_embeds,          
                     negative_pooled_prompt_embeds=negative_pooled_prompt_embeds).images[0]
+    
+    
     dt = time.time() - t0
     fps = 1/dt
     
-    # print(fps)
+    show_fps = meta_input.get(akai_midimix="G3", button_mode="toggle")
+    if show_fps:
+        print(fps)
 
     show_cam = meta_input.get(akai_midimix="H3", akai_lpd8="D1", button_mode="toggle")
-    show_segmask = meta_input.get(akai_midimix="H4", akai_lpd8="D1", button_mode="toggle")
     
     if show_cam:
-        image_show = cam_img
-    elif show_segmask:
-        image_show = np.asarray(cam_img).copy()
-        # image_show[:,:,0] = np.array(human_mask) * 255
+        image_show = cam_img.astype(np.uint8)
     else:
         # just show image_diffusion_mixed for now
         # Re-insert image_diffusion into cam_img at the cropped position
